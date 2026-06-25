950b2fd60f826e577dabffd52175691f47835acf
Khiem Nguyen <nguyenthanhkhiemvn@gmail.com>
2026-06-25 20:51:08 +0300

    99999 JDBC: Fixing ----- DB path jdbc

--- services/patient-service/src/main/java/com/example/patient/PatientServiceApplication.java
+++ services/patient-service/src/main/java/com/example/patient/PatientServiceApplication.java
@@ -1,11 +1,7 @@
 package com.example.patient;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
 @SpringBootApplication
 public class PatientServiceApplication {
 
@@ -15,16 +11,6 @@ public class PatientServiceApplication {
     System.out.println("=================");
 
     SpringApplication.run(PatientServiceApplication.class, args);
-  }
-
-  @Bean
-  CommandLineRunner debugDatasource(Environment env) {
-    return args -> {
-      System.out.println("==== DEBUG DATASOURCE ====");
-      System.out.println("spring.datasource.url = " + env.getProperty("spring.datasource.url"));
-      System.out.println("SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
-      System.out.println("==========================");
-    };
   }
 
 }
