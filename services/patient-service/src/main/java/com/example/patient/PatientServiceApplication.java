5e9ceede80b63f1aedfa7f8f562266759e57b433
Khiem Nguyen <nguyenthanhkhiemvn@gmail.com>
2026-06-25 20:30:08 +0300

    DEBUG SPRING_DATASOURCE_URL url: jdbc: xx

--- services/patient-service/src/main/java/com/example/patient/PatientServiceApplication.java
+++ services/patient-service/src/main/java/com/example/patient/PatientServiceApplication.java
@@ -2,13 +2,18 @@ package com.example.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class PatientServiceApplication {
 
-    public static void main(String[] args) {
-      System.out.println("SPRING_DATASOURCE_URL="
-                + System.getenv("SPRING_DATASOURCE_URL"));
-        SpringApplication.run(PatientServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    ConfigurableApplicationContext ctx = SpringApplication.run(PatientServiceApplication.class, args);
+    Environment env = ctx.getEnvironment();
+    System.out.println("==== DEBUG DATASOURCE ====");
+    System.out.println("spring.datasource.url = " + env.getProperty("spring.datasource.url"));
+    System.out.println("SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
+    System.out.println("==========================");
+  }
 }
