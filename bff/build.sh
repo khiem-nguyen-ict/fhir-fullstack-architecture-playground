18a3f59152a585eb68a895551a4bf99ac46e0072
Khiem Nguyen <nguyenthanhkhiemvn@gmail.com>
2026-06-26 14:02:22 +0300

    Fix URL path for the BFF

---
+++ bff/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Fix the issue for render.com
+
+if [ -n "$PATIENT_SERVICE_URL" ] && ! echo "$PATIENT_SERVICE_URL" | grep -q '^http'; then
+    CONVERTED_URL="https://${PATIENT_SERVICE_URL}.onrender.com"
+    echo "Converting PATIENT_SERVICE_URL: $PATIENT_SERVICE_URL -> $CONVERTED_URL"
+    export PATIENT_SERVICE_URL="${CONVERTED_URL}"
+fi
